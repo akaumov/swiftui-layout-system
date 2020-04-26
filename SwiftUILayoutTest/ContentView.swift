5252c2b3ddec3fb4cb06c0f7da8c3718558a962d
Azat <kaumov.a.r@gmail.com>
2020-04-26 22:19:40 +0300

    Initial Commit

---
+++ SwiftUILayoutTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILayoutTest
+//
+//  Created by Azat Kaiumov on 26.04.2020.
+//  Copyright © 2020 Azat Kaiumov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
